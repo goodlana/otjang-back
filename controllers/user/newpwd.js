82cdac6f22d5c17f0434ef707ce27260d8953c0d
gyu716625 <gyu716625@gmail.com>
2020-07-21 23:05:13 +0900

    :zap: Add change password function
     - 비밀번호를 변경하는 기능을 구현하였습니다.
     - 토큰인증 기간을 test를 위해 임의로 하루로 설정하였습니다.
     - 로그아웃 기능역시 추가하려 했지만 토큰을 클라이언트 부분에서 처리를 해줘야 합니다.
    
    🐛 Fixing  a bug :bug:
    🔥 Removing code or files :fire:
    ✨ Introducing new features :zap:
    💄 Adding or updating style files :lipstick:
    🚧  Work in progress :construction:
    ♻️ Refactoring code :recycle:
    ✏️ Fixing typos :pencil2:
    🚚 Moving or renaming files :truck:
    👽 Updating code due to external API changes :alien:

--- controllers/user/newpwd.js
+++ controllers/user/newpwd.js
@@ -1,5 +1,30 @@
+const { Users } = require('../../models/index');
+
 module.exports = {
     post: (req, res) => {
+        const { id , email } = req.decoded;
+        const { password, newpassword } = req.body;
 
+        console.log('id check: ', id);
+
+        Users
+          .update({ 
+            password: newpassword},
+            { 
+              where: {
+                email: email,
+                password: password,
+            }
+          }).then((result) => {
+              if(!result[0]){
+                res.status(409).json({ message : 'Wrong Password' });
+              } else{
+                res.status(200).json({ message : 'Successful' });
+              }
+          }).catch((err) => {
+              res.status(404).json({ message : err.message });
+          })
+        
+        
     },
 };
--- controllers/user/signin.js
+++ controllers/user/signin.js
@@ -33,8 +33,8 @@ module.exports = {
           const payload = { id: result.id, email: result.email }
 
           const token = jwt.sign(payload, "secret",
-            { // 토큰 유지 기간 설정
-              expiresIn: '10s'//"1 days"
+            { // 토큰 유지 기간 설정 10s' 테스트 완료
+              expiresIn: "1 days"
             });
 
           console.log(token);
