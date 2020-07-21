5111471031e21013faa518c72e4af66108084385
goodlana <lahee13579@gmail.com>
2020-07-21 22:05:42 +0900

    :zap:Item table - delete
    - 특정 아이템을 DB에서 삭제
    
    🐛 Fixing  a bug :bug:
    🔥 Removing code or files :fire:
    ✨ Introducing new features :zap:
    💄 Adding or updating style files :lipstick:
    🚧  Work in progress :construction:
    ♻️ Refactoring code :recycle:
    ✏️ Fixing typos :pencil2:
    🚚 Moving or renaming files :truck:
    👽 Updating code due to external API changes :alien:

--- controllers/items/deleteitem.js
+++ controllers/items/deleteitem.js
@@ -1,5 +1,13 @@
-module.exports = {
-    delete: (req, res) => {
+const { Items } = require('../../models/index')
 
-    },
+module.exports = {
+    delete: (async(req, res) => {
+        const { item_id } = req.params
+        await Items.destroy({
+                where: { id: item_id }
+        })
+        .then(()=> res.status(200).json({"message": "Successful delete"}))
+        .catch(e => res.status(404).send({"message": "Failed", "Error": e}))
+        }
+    )
 };
