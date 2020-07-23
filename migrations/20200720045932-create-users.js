b4c13bdd0a12dc42378eb6c1c1de6c083cb34716
goodlana <lahee13579@gmail.com>
2020-07-24 00:54:34 +0900

    :recycle:add setter in migration's user
    - 마이그레이션 유저js파일에 암호화를 위한 setter를 설정합니다.

--- migrations/20200720045932-create-users.js
+++ migrations/20200720045932-create-users.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+const crypto = require('crypto')
 
 'use strict';
 module.exports = {
@@ -14,7 +15,17 @@ module.exports = {
         type: Sequelize.STRING
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        set : function(val){ //비밀번호 암호화
+          // console.log(hash(val))
+          // console.log(crypto.pbkdf2Sync(val,process.env.PASSWORD_SALT, 1, 32, 'sha512').toString('base64'));
+  
+          // 동기적 실행
+          this.setDataValue('password', crypto.pbkdf2Sync(val, process.env.PASSWORD_SALT, 48537, 64, 'sha512').toString('base64'));
+        },
+        get: function() {
+          return null //GET을 통해서 데이터 조회시 숨김처리 (mysql자체[터미널, 워크벤치 등]에서는 보임)
+        }
       },
       createdAt: {
         allowNull: false,
