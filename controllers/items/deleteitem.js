cbc24b152367718de78269b44fa257d7c35a69f4
goodlana <lahee13579@gmail.com>
2020-07-25 16:21:52 +0900

    :recycle: Refactor deleteitem.js file
    - 아이템을 삭제하면 items_seasons테이블에서도 데이터가 삭제될 수 있도록 구현하였습니다.

--- controllers/items/deleteitem.js
+++ controllers/items/deleteitem.js
@@ -1,10 +1,17 @@
 const { Items } = require('../../models/index')
+const { items_seasons } = require('../../models/index')
 
 module.exports = {
     delete: (async(req, res) => {
         const { item_id } = req.params
         await Items.destroy({
                 where: { id: item_id }
+        })
+
+        await items_seasons.destroy({
+            where:{
+                ItemsId: item_id
+            }
         })
         .then(()=> res.status(200).json({"message": "Successful delete"}))
         .catch(e => res.status(404).send({"message": "Failed", "Error": e}))
